65dc271c06184ac9521b84de28608681a9ea5f2e
Yury <cg@tut.by>
2021-06-06 21:41:28 +0300

    практика ч2

--- 12/script.js
+++ 12/script.js
@@ -36,13 +36,31 @@ const personalMovieDB = {
 
 };
 
-const a = prompt('Один из последних просмотренных фильмов?','');
-const b = prompt('На сколько оцените его?','');
-const c = prompt('Один из последних просмотренных фильмов?','');
-const d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+        b = prompt('На сколько оцените его?','');
+      
+
+        if (a != null && b !=null && a !='' && b !='' && a.length<50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }else{
+            console.log('error');
+            i--;
+        }
+    
+}
+
+if (personalMovieDB.count<10) {
+    console.log('просмотрено мало фильмов ');
+} else if (personalMovieDB.count>=10 && personalMovieDB.count<30) {
+    console.log('вы класический зритель ');
+    
+} else  {
+    console.log('произошла ошибка ');
+}
 
 console.log(personalMovieDB);
 
